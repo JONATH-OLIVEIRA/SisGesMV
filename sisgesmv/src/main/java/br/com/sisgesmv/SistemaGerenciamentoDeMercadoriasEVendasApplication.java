e0949f158599e96e16782db3773afdc860d10924
Jonath Oliveira <akiles22a@gmail.com>
2025-05-26 12:56:12 -0300

    criacao projeto

---
+++ sisgesmv/src/main/java/br/com/sisgesmv/SistemaGerenciamentoDeMercadoriasEVendasApplication.java
@@ -0,0 +1,13 @@
+package br.com.sisgesmv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGerenciamentoDeMercadoriasEVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGerenciamentoDeMercadoriasEVendasApplication.class, args);
+	}
+
+}
---
+++ sisgesmv/src/test/java/br/com/sisgesmv/SistemaGerenciamentoDeMercadoriasEVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sisgesmv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGerenciamentoDeMercadoriasEVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
